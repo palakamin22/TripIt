79ccddac26d85e49f798ad2bd15ef6c8b6008058
Palak <palakamin2206@gmail.com>
2020-02-18 21:14:09 +0530

    first commit

---
+++ app/src/main/java/com/example/tripit/FirstPage.java
@@ -0,0 +1,17 @@
+package com.example.tripit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tripit.R;
+
+public class FirstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+
+    }
+}
---
+++ app/src/main/java/com/example/tripit/ui/place/PlaceFragment.java
@@ -0,0 +1,35 @@
+package com.example.tripit.ui.place;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.tripit.R;
+
+public class PlaceFragment extends Fragment {
+
+    private PlaceViewModel placeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        placeViewModel =
+                ViewModelProviders.of(this).get(PlaceViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_place, container, false);
+        final TextView textView = root.findViewById(R.id.text_place);
+        placeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
